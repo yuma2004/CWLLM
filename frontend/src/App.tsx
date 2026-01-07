b2acd05797d8cbd7f23f9a87d75e69380806a9c3
ゆうま <101465220+yuma2004@users.noreply.github.com>
2026-01-07 22:02:11 +0900

    Add lazy-loaded routes with suspense

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,23 @@
+import { lazy, Suspense } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import Layout from './components/Layout'
-import Home from './pages/Home'
-import Login from './pages/Login'
 import ProtectedRoute from './components/ProtectedRoute'
-import Companies from './pages/Companies'
-import CompanyDetail from './pages/CompanyDetail'
-import ChatworkSettings from './pages/ChatworkSettings'
-import Tasks from './pages/Tasks'
-import TaskDetail from './pages/TaskDetail'
-import Projects from './pages/Projects'
-import ProjectDetail from './pages/ProjectDetail'
-import Wholesales from './pages/Wholesales'
-import WholesaleDetail from './pages/WholesaleDetail'
-import Settings from './pages/Settings'
-import Exports from './pages/Exports'
+import LoadingState from './components/ui/LoadingState'
+
+const Home = lazy(() => import('./pages/Home'))
+const Login = lazy(() => import('./pages/Login'))
+const Companies = lazy(() => import('./pages/Companies'))
+const CompanyDetail = lazy(() => import('./pages/CompanyDetail'))
+const ChatworkSettings = lazy(() => import('./pages/ChatworkSettings'))
+const Tasks = lazy(() => import('./pages/Tasks'))
+const TaskDetail = lazy(() => import('./pages/TaskDetail'))
+const Projects = lazy(() => import('./pages/Projects'))
+const ProjectDetail = lazy(() => import('./pages/ProjectDetail'))
+const Wholesales = lazy(() => import('./pages/Wholesales'))
+const WholesaleDetail = lazy(() => import('./pages/WholesaleDetail'))
+const Settings = lazy(() => import('./pages/Settings'))
+const Exports = lazy(() => import('./pages/Exports'))
 
 type ProtectedRouteConfig = {
   path: string
@@ -46,12 +49,14 @@ const renderProtectedRoute = (route: ProtectedRouteConfig) => (
 function App() {
   return (
     <AuthProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {protectedRoutes.map((route) => (
-          <Route key={route.path} path={route.path} element={renderProtectedRoute(route)} />
-        ))}
-      </Routes>
+      <Suspense fallback={<LoadingState />}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          {protectedRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={renderProtectedRoute(route)} />
+          ))}
+        </Routes>
+      </Suspense>
     </AuthProvider>
   )
 }
